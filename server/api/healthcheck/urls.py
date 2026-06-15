06190e0b783a3999ffb77bb0a4205156da6a2fb2
Games4Doritos <evanmiocevich@gmail.com>
2026-06-15 18:11:41 +0800

    - Removed server/desv.sh, was unneeded as it's a one line command - Changed header and title for the admin dashboard, will move onto styling next - Updated MIT license to 2026 - Updated npm packages, there are still 2 vulnerabilities so will need to configure

--- server/api/healthcheck/urls.py
+++ server/api/healthcheck/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 app_name = "healthcheck"
 urlpatterns = [
     path("ping/", views.ping, name="ping"),
 ]
+
+# Override styling of the admin dashboard here
+admin.site.site_header = "Agronomy Club - Admin Dashboard"
+admin.site.site_title = "Site administration | Agronomy Club"
