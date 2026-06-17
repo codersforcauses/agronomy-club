a1d7417cde986b35bbd6cce0bd624595d6c88596
Games4Doritos <evanmiocevich@gmail.com>
2026-06-17 20:23:56 +0800

    Change queryClient Declaration

--- client/src/components/main/providers.tsx
+++ client/src/components/main/providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   /*const [queryClient] = useState(() => new QueryClient({
@@ -11,7 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         },
         },
     }))*/
-  const [queryClient] = React.useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
