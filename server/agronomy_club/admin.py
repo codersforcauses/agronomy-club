5fbfdeefa6811a3e57e0e13161e4560fd802eebc
Nhat Minh Do <140363490+zephris@users.noreply.github.com>
2026-06-23 15:31:51 +0800

    chore: style linting fixes

--- server/agronomy_club/admin.py
+++ server/agronomy_club/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin  # noqa
 from api.chapters.models import Chapters
 
+
 # Register your models here.
 @admin.register(Chapters)
 class ChaptersAdmin(admin.ModelAdmin):
