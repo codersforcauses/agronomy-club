511937d2d935b3a9de6e898e9635b5a84595819f
Nhat Minh Do <140363490+zephris@users.noreply.github.com>
2026-06-23 15:28:49 +0800

    register model with django, customise model view in admin panel

--- server/agronomy_club/admin.py
+++ server/agronomy_club/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin  # noqa
+from api.chapters.models import Chapters
 
 # Register your models here.
+@admin.register(Chapters)
+class ChaptersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'abbrev', 'location', 'email')
+    search_fields = ('id', 'name', 'abbrev', 'location')
+    pass
