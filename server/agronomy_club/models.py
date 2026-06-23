4d44ebca286e7d168f8ded1cc74a04c19fe50664
Anthony Do <140363490+zephris@users.noreply.github.com>
2026-06-23 20:08:49 +0800

    [Issue 18] Added django chapter model (#28)
    * Added django chapter model
    
    * minor Flake8 styling lint fix
    
    * newline at end of file
    
    * add colorfield dependency package and changed model to match new ERD requirements
    
    * linting change, added description text limit
    
    * Added description text limit
    
    * changed primary key to be editable and logo data type
    
    * register model with django, customise model view in admin panel
    
    * chore: style linting fixes
    
    * altered file structure to fit new requirements, further linting fix
    
    * fixed error on random color generator function
    
    * chore: style linting
    
    * fixed color generator functions and added new migration for chapter model

--- server/agronomy_club/models.py
+++ server/agronomy_club/models.py
@@ -1,3 +1,34 @@
 from django.db import models  # noqa
+from colorfield.fields import ColorField  # noqa
+# Model for chapters information such as logo, location, description and email. Chapter members should be stored in a seperate model.
+# This model uses django-colorfield to store the colour of the chapter as well as provide a color picker widget in admin panel.
+# Documentation can be found here: https://github.com/fabiocaccamo/django-colorfield#readme
 
-# Create your models here.
+
+def generate_random_hex():
+    import random
+
+    return "#{:06x}".format(random.randint(0, 0xFFFFFF))  # Generate random hex colour code
+
+
+# Should only be ran once when new chapter is created with no provided color.
+# Can also be delegated to the form if needed instead of the model, but this is more convenient for now.
+def random_color():
+    while True:
+        new_color = generate_random_hex()
+
+        # If color does not exist yet in database then return it, otherwise generate a new one
+        if not Chapters.objects.filter(colour=new_color).exists():
+            return new_color
+
+
+class Chapters(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True, unique=True)
+    name = models.CharField(max_length=255)
+    abbrev = models.CharField(max_length=255)
+    # Store chapter logos in media/chapter_logos/ directory. Need to also include default logo for when chapter does not provide one.
+    logo = models.ImageField(upload_to='chapter_logos/', null=True, blank=True, default='chapter_logos/default.png')
+    location = models.CharField(max_length=255)
+    desc = models.TextField(max_length=5000)
+    email = models.EmailField(max_length=255)
+    colour = ColorField(default=random_color, unique=True, editable=True)  # lambda function used to generate new random color
