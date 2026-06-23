b85b250c53c0c20609e74ddc17e8e43c9806502e
Nhat Minh Do <140363490+zephris@users.noreply.github.com>
2026-06-23 15:17:30 +0800

    linting change, added description text limit

--- server/api/chapters/models.py
+++ server/api/chapters/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from colorfield.fields import ColorField  
-
+from colorfield.fields import ColorField
 
 # Model for chapters information such as logo, location, description and email. Chapter members should be stored in a seperate model.
 # This model uses django-colorfield to store the colour of the chapter as well as provide a color picker widget in admin panel.
 # Documentation can be found here: https://github.com/fabiocaccamo/django-colorfield#readme
+
+
 class Chapters(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True, editable=False, unique=True)
     name = models.CharField(max_length=255)
@@ -13,15 +14,13 @@ class Chapters(models.Model):
     location = models.CharField(max_length=255)
     desc = models.TextField()
     email = models.EmailField(max_length=255)
-    colour = ColorField(default=lambda: Chapters.random_color(), unique=True, editable=True) # lambda function used to generate new random color
-
+    colour = ColorField(default=lambda: Chapters.random_color(), unique=True, editable=True)  # lambda function used to generate new random color
 
     @staticmethod
     def generate_random_hex():
         import random
-        
-        return "#{:06x}".format(random.randint(0, 0xFFFFFF)) # Generate random hex colour code
 
+        return "#{:06x}".format(random.randint(0, 0xFFFFFF))  # Generate random hex colour code
 
     # Should only be ran once when new chapter is created with no provided color.
     # Can also be delegated to the form if needed instead of the model, but this is more convenient for now.
@@ -29,6 +28,7 @@ class Chapters(models.Model):
     def random_color():
         while True:
             new_color = Chapters.generate_random_hex()
-            if not Chapters.objects.filter(colour=new_color).exists(): # If color does not exist yet in database then return it, otherwise generate a new one
+
+            # If color does not exist yet in database then return it, otherwise generate a new one
+            if not Chapters.objects.filter(colour=new_color).exists():
                 return new_color
-        
