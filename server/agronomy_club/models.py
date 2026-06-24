8c7434f9e27787f85d096686e35ab189adec1835
Johar Khan <joharalm@gmail.com>
2026-06-24 09:54:31 +0000

    Add fields as per new ERD

--- server/agronomy_club/models.py
+++ server/agronomy_club/models.py
@@ -5,3 +5,5 @@ class Quiz(models.Model):
     name = models.CharField(max_length=30)
     public = models.BooleanField()
     chapter = models.ForeignKey("Chapter", on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    quiz_data = models.JSONField()
