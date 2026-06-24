8dc5095cb8d9c25c05a00089f025e6c5f8841126
Johar Khan <joharalm@gmail.com>
2026-06-24 09:43:40 +0000

    Create Quiz model

--- server/agronomy_club/models.py
+++ server/agronomy_club/models.py
@@ -1,3 +1,7 @@
 from django.db import models  # noqa
 
 # Create your models here.
+class Quiz(models.Model):
+    name = models.CharField(max_length=30)
+    public = models.BooleanField()
+    chapter = models.ForeignKey("Chapter", on_delete=models.CASCADE)
