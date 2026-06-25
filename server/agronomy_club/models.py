20a5d90ccd131ad2b0ec95eed330eddb88b47198
Nick A <nickalexander8257@gmail.com>
2026-06-25 04:27:12 +0000

    add a color for the tag

--- server/agronomy_club/models.py
+++ server/agronomy_club/models.py
@@ -53,6 +53,7 @@ class Event(models.Model):
 # Resource type tags for filter
 class ResourceTypeTag(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    color = ColorField(default=random_color, unique=True, editable=True)
 
     def __str__(self):
         return str(self.name)
