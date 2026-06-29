fdc620ed542182ca790516f4411109e96915efa9
nick <nickalexander8257@gmail.com>
2026-06-29 18:11:55 +0000

    URL patterns

--- server/agronomy_club/urls.py
+++ server/agronomy_club/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path("ping/", views.ping, name="ping"),
+
+    # Resources
+    path("resources/", views.ResourceListCreateAPIView.as_view(), name="resource-list-create"),
+    path("resources/<int:pk>/", views.ResourceRetreiveUpdateDestroyAPIView.as_view(), name="resource-detail"),
+
+    # Resource type tags
+    path("resource-type-tags/", views.ResourceTypeTagListCreateAPIView.as_view(), name="resource-type-tag-list-create"),
+    path("resource-type-tags/<int:pk>/", views.ResourceTypeTagRetreiveUpdateDestroyAPIView.as_view(), name="resource-type-tag-detail"),
 ]
 
 # Override styling of the admin dashboard here
