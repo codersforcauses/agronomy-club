dca02de071fd992f5c9759f6ad00d8c92cbf59a4
nick <nickalexander8257@gmail.com>
2026-06-29 17:06:16 +0000

    add serializers for resource and type tags model

---
+++ server/agronomy_club/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Resource, ResourceTypeTag
+
+
+class ResourceTypeTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResourceTypeTag
+        fields = ['id', 'name', 'color']
+
+
+class ResourceSerializer(serializers.ModelSerializer):
+    # type tag serializer for read request (show name and color)
+    type_tags = ResourceTypeTagSerializer(many=True, read_only=True)
+
+    # type tag serializer for write request
+    type_tag_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=ResourceTypeTag.objects.all(),
+        source='type_tags',
+        write_only=True,
+        required=False,
+    )
+
+    class Meta:
+        model = Resource
+        fields = ['id', 'chapter', 'name', 'link', 'upload_date', 'type_tags', 'type_tag_ids']
