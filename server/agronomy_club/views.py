06129b211848006ad5c71405124ffa0cf1bd4468
nick <nickalexander8257@gmail.com>
2026-06-29 17:59:19 +0000

    add base generic views

--- server/agronomy_club/views.py
+++ server/agronomy_club/views.py
@@ -1,4 +1,8 @@
 from rest_framework.decorators import api_view
+from rest_framework import generics
+
+from .models import Resource, ResourceTypeTag
+from .serializers import ResourceSerializer, ResourceTypeTagSerializer
 
 from django.http import HttpResponse
 
@@ -7,3 +11,45 @@ from django.http import HttpResponse
 @api_view(["GET"])
 def ping(request):
     return HttpResponse("Pong!", status=200)
+
+
+class ResourceTypeTagList(generics.ListAPIView):
+    queryset = ResourceTypeTag.objects.all()
+    serializer_class = ResourceTypeTagSerializer
+
+    # TODO: guest permission
+
+
+class ResourceTypeTagCreate(generics.CreateAPIView):
+    queryset = ResourceTypeTag.objects.all()
+    serializer_class = ResourceTypeTagSerializer
+
+    # TODO: super admin permission
+
+
+class ResourceTypeTagRetreiveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ResourceTypeTag.objects.all()
+    serializer_class = ResourceTypeTagSerializer
+
+    # TODO: super admin permission
+
+
+class ResourceList(generics.ListAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+    # TODO: guest permission
+
+
+class ResourceCreate(generics.CreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+    # TODO: chapter admin permission
+
+
+class ResourceRetreiveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+    # TODO: chapter admin permission
