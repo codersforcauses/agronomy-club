dd27c4d3318979e935ecb13d06767fc9ac15bd1a
Dyutiman <dyutimanshiv@gmail.com>
2026-07-04 07:14:40 +0000

    test fixes

--- server/agronomy_club/tests.py
+++ server/agronomy_club/tests.py
@@ -280,4 +280,4 @@ class ResourceAPISmokeTests(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 19)
+        self.assertEqual(len(response.json()), 17)
