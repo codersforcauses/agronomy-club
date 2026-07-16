960078ccc81ef2fb6cebf0dd5e806a027d7639d2
Caroline <cayrolann23@gmail.com>
2026-07-16 09:57:55 +0000

    Added `useChapters` hook to `useChapters.ts`
    referenced `useQuizzes.ts` and `useChapter.ts`

---
+++ client/src/hooks/useChapters.ts
@@ -0,0 +1,33 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+import api from "@/lib/api";
+
+export type ApiChapterList = {
+  id: number;
+  name: string;
+  abbrev: string;
+  logo: string | null;
+  location: string;
+  desc: string;
+  colour: string;
+};
+
+export const useChapters = (
+  args?: Omit<
+    UseQueryOptions<ApiChapterList[], AxiosError>,
+    "queryKey" | "queryFn"
+  >,
+) => {
+  return useQuery<ApiChapterList[], AxiosError>({
+    ...args,
+    queryKey: ["chapters"],
+    queryFn: () => api.get("/chapters/").then((res) => res.data),
+    retry: (failureCount, error) => {
+      if (error?.response?.status === 404) {
+        return false;
+      }
+      return failureCount < 3;
+    },
+  });
+};
