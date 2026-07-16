b7342313e59b246eed8d4757cd6a7197ce8f241d
nick <nickalexander8257@gmail.com>
2026-07-16 10:58:39 +0000

    add a tags filter to hook

--- client/src/hooks/useResources.ts
+++ client/src/hooks/useResources.ts
@@ -18,11 +18,15 @@ export type ApiResource = {
   type_tags: ApiResourceTypeTag[];
 };
 
-export function useResource() {
+export function useResource(tags?: number[]) {
   return useQuery<ApiResource[], AxiosError>({
-    queryKey: ["resources"],
+    queryKey: ["resources", { tags }],
     queryFn: async () => {
-      const response = await api.get<ApiResource[]>("/resources/");
+      const response = await api.get<ApiResource[]>("/resources/", {
+        params: {
+          tags: tags?.length ? tags.join(",") : undefined,
+        },
+      });
       return response.data;
     },
     retry: (failureCount, error) => {
