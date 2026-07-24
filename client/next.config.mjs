28f4eb32cd14d54d4146602a37b6992b2baa6cea
nick <nickalexander8257@gmail.com>
2026-07-24 02:24:52 +0000

    allow localhost to be an image source

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,16 @@ const isWindowsDevContainer = () =>
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+    ],
+  },
   // dumb fix for windows docker
   webpack: isWindowsDevContainer()
     ? (config) => {
